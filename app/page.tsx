6333a6f7c7200e163f16aa73d0e7391a5da999cd
Angus McCasker <angus@example.com>
2025-06-25 12:01:34 +1000

    Clean up code for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 import InfluencerDashboard from "../dashboard"
 
 export default function Page() {
-  return (
-    <div>
-      <div style={{padding: '20px', background: 'red', color: 'white', textAlign: 'center'}}>
-        🚨 DEPLOYMENT TEST - If you see this red banner, the deployment is working! 🚨
-      </div>
-      <InfluencerDashboard />
-    </div>
-  )
+  return <InfluencerDashboard />
 }
